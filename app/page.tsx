abde65d65e5f749d14979cccd5acab356a173033
0xOmen <0xOmen21@gmail.com>
2024-04-11 06:23:34 -0600

    2

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ const frameMetadata = getFrameMetadata({
       label: "Begin",
     },
   ],
-  image: `${process.env.NEXT_PUBLIC_GATEWAY_URL}/Phenomenon.jpg`,
+  image: `${process.env.NEXT_PUBLIC_GATEWAY_URL}/Gameplay.png`,
   post_url: `${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=1`,
 });
 
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: "Phenomenon",
     description: "THE blockchain based social strategy game",
-    images: [`${process.env.NEXT_PUBLIC_GATEWAY_URL}/Phenomenon.jpg`],
+    images: [`${process.env.NEXT_PUBLIC_GATEWAY_URL}/Gameplay.png`],
   },
   other: {
     ...frameMetadata,
